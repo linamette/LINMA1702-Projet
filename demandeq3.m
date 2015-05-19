6fc8735f09b9bd85d75a4a4e38caaa8d509aaa86
pal <pierre-alexandre@student.uclouvain.be>
2015-05-19 10:43:04 +0200

    Add a graph

--- demandeq3.m
+++ demandeq3.m
@@ -1,10 +1,13 @@
-function [fval,x,f,A,b,Aeq,beqvrai]= demandeq3( donnees ,epsilon)
+function [fval,x,f,A,b,Aeq,beqvrai]= demandeq3( donnees ,epsilon,show)
 
 if nargin <2
     epsilon = 0;
 end
+if nargin <3
+    show = 0;
+end
 %parametre d'optimisation
-semaine = 15;
+semaine = donnees.T;
 d_t = donnees.demande;
 c_m = donnees.cout_materiaux;
 c_s = donnees.cout_stockage;
@@ -31,11 +34,11 @@ nhs_max = n_hs * n_o /d_a;
 %création de f à optimiser
 f=zeros(5*semaine+1,1);
 for i=1:semaine
-   f(i,1)=c_m; 
+    f(i,1)=c_m;
 end
 
 for i=semaine+1:2*semaine
-   f(i,1)=c_m + d_a  * c_hs;
+    f(i,1)=c_m + d_a  * c_hs;
 end
 
 for i=2*semaine+1: 3*semaine
@@ -43,11 +46,11 @@ for i=2*semaine+1: 3*semaine
 end
 
 for i=3*semaine+2:4*semaine+1
-   f(i,1)=c_s; 
+    f(i,1)=c_s;
 end
 
 for i=4*semaine+2:5*semaine+1
-   f(i,1)=c_r; 
+    f(i,1)=c_r;
 end
 
 
@@ -56,40 +59,40 @@ end
 %création de la matrice A
 A=zeros(9*semaine,5*semaine+1);
 for i=1:semaine-1 %r^(t+1) <= d_t(i)
-
-  A(i, i+4*semaine+2)= 1;
-end 
+    
+    A(i, i+4*semaine+2)= 1;
+end
 
 
 count=1;
 for i=semaine : 4*semaine-1 % contrainte n_t <= n_max + nhs_t <= nhs_max + nst <= n_max_st
     A(i,count)=1;
-    count = count + 1; 
-end 
+    count = count + 1;
+end
 
 count=1;
 for i=4*semaine:9*semaine %toutes les variables sont positives
-   A(i,count)=-1;
-   count = count +1;
+    A(i,count)=-1;
+    count = count +1;
 end
 
 
 %création de la matrice b
 b=zeros(9*semaine,1);
 for i=1:semaine-1 %r^(t+1) <= d_t(i)
-  b(i)=d_t(i);
+    b(i)=d_t(i);
 end
-for i=semaine:(2*semaine-1) %n_t <= n_max 
+for i=semaine:(2*semaine-1) %n_t <= n_max
     b(i)= n_max;
-   
+    
 end
 
 for i =2*semaine: 3*semaine-1 %nst <= n_max_st
-    b(i)= nhs_max; 
+    b(i)= nhs_max;
 end
 
 for i=3*semaine:4*semaine-1 %les variables doivent être positives
- b(i)=n_max_st;
+    b(i)=n_max_st;
 end
 
 %Contraintes telles Aeq *x =beq.'
@@ -98,23 +101,23 @@ end
 
 Aeq=zeros(semaine+3,5*semaine+1);
 for i=1:semaine-1  %n_t + nhs_t +ns_t + s_t-1 - s_t -r_t +r_t+1 = d_t(i)
-   Aeq(i ,i)=1;
-   Aeq(i, i+semaine)=1;
-   Aeq(i, i+2*semaine)=1;
-   Aeq(i, i+3*semaine)=1;
-   Aeq(i, i+3*semaine+1)=-1;
-   Aeq(i, i+4*semaine+1)=-1;
-   Aeq(i, i+4*semaine+2)=1;
+    Aeq(i ,i)=1;
+    Aeq(i, i+semaine)=1;
+    Aeq(i, i+2*semaine)=1;
+    Aeq(i, i+3*semaine)=1;
+    Aeq(i, i+3*semaine+1)=-1;
+    Aeq(i, i+4*semaine+1)=-1;
+    Aeq(i, i+4*semaine+2)=1;
 end %premiere contrainte pour les semaines sauf la derniere
 
 for i=semaine:semaine
-   Aeq(i ,i)=1;
-   Aeq(i, i+semaine)=1;
-   Aeq(i, i+2*semaine)=1;
-   Aeq(i, i+3*semaine)=1;
-   Aeq(i, i+3*semaine+1)=-1;
-   Aeq(i, i+4*semaine+1)=-1;
-  
+    Aeq(i ,i)=1;
+    Aeq(i, i+semaine)=1;
+    Aeq(i, i+2*semaine)=1;
+    Aeq(i, i+3*semaine)=1;
+    Aeq(i, i+3*semaine+1)=-1;
+    Aeq(i, i+4*semaine+1)=-1;
+    
 end %premiere contrainte pour la derniere semaine
 
 Aeq(semaine+1,3*semaine+1)=1; %s_0 = s_i
@@ -123,23 +126,43 @@ Aeq(semaine+3,4*semaine+2)=1; %r_1 = 0
 
 beq = zeros(1,semaine+3); %n_t + nhs_t +ns_t + s_t-1 - s_t -r_t +r_t+1 = d_t(i)
 for i=1:semaine
-   beq(i)=d_t(i); 
+    beq(i)=d_t(i);
 end
 for i=semaine+1:semaine+2 %s_0 = s_i + s_t = s_i
-   beq(i)=s_i; 
+    beq(i)=s_i;
 end
 for i=semaine+3:semaine+3 %r_1 = 0
-   beq(i)=0; 
+    beq(i)=0;
 end
 
-beqvrai=beq.'
+beqvrai=beq.';
 
 options=optimoptions(@linprog,'Algorithm','dual-simplex');
 [x,fval,exiflag, output, lambda]=linprog(transpose(f),A,b,Aeq,beq.',[],[],[],options);
 fval=fval+c_h*n_o*semaine*35; %fval renvoie le cout réel, on ajoute une constante
-                              %qui représente les salaires de tous les
-                              %employes sur la durée désirée
+                                %qui représente les salaires de tous les
+                                %employes sur la durée désirée
 
+
+%Montre l'évolution des valeurs en fonction des semaines
+if show ~=0
+    figure();
+    hold on;
+    plot((1:1:15),d_t,'g');
+    plot((1:1:15),x(1:semaine),'b');
+    plot((1:1:15),x(semaine+1:2*semaine),'r');
+    plot((1:1:15),x(2*semaine+1:3*semaine),'m');
+    plot((1:1:15),x(3*semaine+2:4*semaine+1),'c');
+    plot((1:1:15),x(4*semaine+2:5*semaine+1),'k');
+    xlabel('semaine');
+    title('Nombres de smartphones produits en fonctions des semaines pour une certaine demande');
+    ylabel('nombres d unités produites');
+    legend('demande','normal','heures supp','sous-traités','stockés','retardés');
+    
+    xlim([1 15]);
+    hold off;
+    
+end
 
 
 end
